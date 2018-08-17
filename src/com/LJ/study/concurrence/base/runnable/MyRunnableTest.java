9a32672586034ae160229e41c47ad7a94cf97d69
liujunj <yonyou2017>
2018-08-17 14:53:32 +0800

    添加并发练习代码

--- src/com/LJ/study/concurrence/base/runnable/MyRunnableTest.java
+++ src/com/LJ/study/concurrence/base/runnable/MyRunnableTest.java
@@ -15,8 +15,10 @@ public class MyRunnableTest {
         MyThread t1 = new MyThread();
         MyThread t2 = new MyThread();
         //启动线程
-        t1.start();
-        t2.start();
+//        t1.start();
+        t1.run();
+//        t2.start();
+        t2.run();//直接调用run并没有启动新的现场，仍在main线程中。
 
         for (int i = 0; i < 3; i++)
         {
--- src/com/LJ/study/concurrence/base/runnable/MyThread.java
+++ src/com/LJ/study/concurrence/base/runnable/MyThread.java
@@ -3,7 +3,7 @@ package com.LJ.study.concurrence.base.runnable;
 public class MyThread extends Thread{
     public void run(){
         for (int i = 0; i < 3; i++) {
-            System.out.println(this.getName() + "在运行");
+            System.out.println(Thread.currentThread().getName() + "在运行");
         }
     }
 }
