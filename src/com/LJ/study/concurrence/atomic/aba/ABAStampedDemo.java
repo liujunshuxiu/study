7bc7ad119eac6f90619e3cbffa231cb0a0de67b1
liujunj <liujunj@yonyou.com>
2018-09-09 21:27:49 +0800

    提交线程练习的代码-atomic类练习3

---
+++ src/com/LJ/study/concurrence/atomic/aba/ABAStampedDemo.java
@@ -0,0 +1,87 @@
+package com.LJ.study.concurrence.atomic.aba;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ *AtomicStampedReference原子类是一个带有时间戳的对象引用
+ * 用于处理unsafe的aba问题，内部有一个时间戳比对
+ */
+public class ABAStampedDemo {
+    static AtomicInteger atIn = new AtomicInteger(100);
+
+    //初始化时需要传入一个初始值和初始时间
+    static AtomicStampedReference<Integer> atomicStampedR =
+            new AtomicStampedReference<Integer>(200,0);
+
+
+    static Thread t1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            //更新为200
+            atIn.compareAndSet(100, 200);
+            //更新为100
+            atIn.compareAndSet(200, 100);
+        }
+    });
+
+
+    static Thread t2 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean flag=atIn.compareAndSet(100,500);
+            System.out.println("flag:"+flag+",newValue:"+atIn);
+        }
+    });
+
+
+    static Thread t3 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            int time=atomicStampedR.getStamp();
+            //更新为200
+            atomicStampedR.compareAndSet(100, 200,time,time+1);
+            //更新为100
+            int time2=atomicStampedR.getStamp();
+            atomicStampedR.compareAndSet(200, 100,time2,time2+1);
+        }
+    });
+
+
+    static Thread t4 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            int time = atomicStampedR.getStamp();
+            System.out.println("sleep 前 t4 time:"+time);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean flag=atomicStampedR.compareAndSet(100,500,time,time+1);
+            System.out.println("flag:"+flag+",newValue:"+atomicStampedR.getReference());
+        }
+    });
+
+    public static  void  main(String[] args) throws InterruptedException {
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        t3.start();
+        t4.start();
+        /**
+         * 输出结果:
+         flag:true,newValue:500
+         sleep 前 t4 time:0
+         flag:false,newValue:200
+         */
+    }
+}
