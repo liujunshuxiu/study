2db120556272869b6f7155bd5c77c324e43887e9
liujunj <liujunj@yonyou.com>
2018-10-19 09:23:10 +0800

    终于找到看map的好方式，复写hashcode方法，哇咔咔，我怎么如此机制

---
+++ src/main/java/study/MapStudyKey.java
@@ -0,0 +1,23 @@
+package study;
+
+public class MapStudyKey {
+
+    private int type;
+
+
+    public MapStudyKey() {
+    }
+
+    public MapStudyKey(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        int a = 1;
+        return a;
+    }
+
+
+
+}
