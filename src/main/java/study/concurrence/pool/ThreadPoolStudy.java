f81edb2bbd61ddd034c71300f74d56deaa824ff7
liujunj <liujunj@yonyou.com>
2018-11-12 12:24:50 +0800

    上传线程池的学习代码

---
+++ src/main/java/study/concurrence/pool/ThreadPoolStudy.java
@@ -0,0 +1,53 @@
+package study.concurrence.pool;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolStudy {
+
+
+    
+    
+    
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    System.out.println(Thread.currentThread().getName() + " run");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(6, 10, 5, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+
+
+        executor.execute(myRunnable);
+        executor.execute(myRunnable);
+        executor.execute(myRunnable);
+        System.out.println("---先开三个---");
+        System.out.println("核心线程数" + executor.getCorePoolSize());
+        System.out.println("线程池数" + executor.getPoolSize());
+        System.out.println("队列任务数" + executor.getQueue().size());
+        executor.execute(myRunnable);
+        executor.execute(myRunnable);
+        executor.execute(myRunnable);
+        System.out.println("---再开三个---");
+        System.out.println("核心线程数" + executor.getCorePoolSize());
+        System.out.println("线程池数" + executor.getPoolSize());
+        System.out.println("队列任务数" + executor.getQueue().size());
+        Thread.sleep(8000);
+        System.out.println("----8秒之后----");
+        System.out.println("核心线程数" + executor.getCorePoolSize());
+        System.out.println("线程池数" + executor.getPoolSize());
+        System.out.println("队列任务数" + executor.getQueue().size());
+    }
+
+
+}
