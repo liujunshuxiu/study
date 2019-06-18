0d430d3bc2b04c087b44a7f676d1ecfec6027a7b
liujunj <liujunj@yonyou.com>
2019-06-18 17:47:48 +0800

    methodHandle方法学习代码

--- src/main/java/study/jvm/MethodHandleTest.java
+++ src/main/java/study/jvm/MethodHandleTest.java
@@ -1,5 +1,32 @@
 package study.jvm;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+/**
+ * jvm动态类型语言支持
+ * invokedynamic字节码指令
+ * 有点像lamda的Function
+ */
 public class MethodHandleTest {
+    static class ClassA{
+        public void println(String s){
+            System.out.println(s);
+        }
+    }
+
+
+    private static MethodHandle getPrintMH(Object reveiver) throws Throwable{
+        // 接收数组，返回一个List对象
+//        MethodType mt = MethodType.methodType(List.class, Object[].class);
+        MethodType mt = MethodType.methodType(void.class,String.class);
+        return MethodHandles.lookup().findVirtual(reveiver.getClass(),"println",mt).bindTo(reveiver);
+    }
+
+    public static void main(String[] args) throws Throwable{
+        Object obj = System.currentTimeMillis() % 2 == 0 ? System.out : new ClassA();
+        getPrintMH(obj).invoke("icyfenixghf");
+    }
 
 }
