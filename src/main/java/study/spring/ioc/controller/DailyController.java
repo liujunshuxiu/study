4fe6b87a79c56f0b5b6af87594715f6cdbb1055e
liujunj <yonyou2017>
2019-09-17 19:25:03 +0800

    上传死循环，用来测试jvm学习

---
+++ src/main/java/study/spring/ioc/controller/DailyController.java
@@ -0,0 +1,24 @@
+package study.spring.ioc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "bookController")
+public class DailyController {
+
+    static class TestObject{}
+
+    @RequestMapping(value = "/dead")
+    public void dead()
+    {
+        List<TestObject> list=new ArrayList<TestObject>();
+        while(true){
+            list.add(new TestObject());
+        }
+
+    }
+}
