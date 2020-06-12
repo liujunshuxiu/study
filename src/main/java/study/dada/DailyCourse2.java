5bbc6f526038c73c7e5069ed4c0854d87ab3aeb2
jun.liu <jun.liu@dadaabc.net>
2020-06-12 17:08:48 +0800

    提交小组学习代码course2，版本1

---
+++ src/main/java/study/dada/DailyCourse2.java
@@ -0,0 +1,51 @@
+package study.dada;
+
+public class DailyCourse2 {
+
+    private static int[] str = new int[]{9,65,1,36,4,3,5,7,9};
+
+    private static int k = 4;
+
+//    public static void main(String[] args) {
+//        int[] res = new int[k];
+//
+//        System.out.println(res.length);
+//        System.out.println(res[k -1]);
+//
+//        for(int i = 0 ; i < str.length ; i++){
+//            if(str[i] >= res[k -1] ){
+//                add2Str(str[i],res);
+//            }
+//        }
+//
+//        System.out.println("Max k is :"+ res[k -1]);
+//    }
+
+    private static void add2Str(int i, int[] res) {
+
+        for(int j = k -1 ;j >= 0 ;j--){
+            if(i >= res[j]){
+                continue;
+            }else {
+                //插入i，数字右移一位
+                moveRes(i,j,res);
+            }
+        }
+    }
+
+    private static void moveRes(int i, int j, int[] res) {
+        int m = 0;
+        int a = 0;
+        for(int n = j ; n < res.length ; n++){
+            a = res[j + 1];
+            res[j + 1] = res[j];
+            res[j] = m;
+            m = a;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] res = new int[]{5,4,3,2,1};
+        moveRes(4,1,res);
+    }
+}
